b058d3a1061720554ab9895548761a11aea02530
Jerome Lacube <jerome.lacube@gmail.com>
2025-06-10 11:00:42 +0000

    Bumped to imagen4 and  veo3

--- models/models.py
+++ models/models.py
@@ -24,15 +24,15 @@ def get_mistral_ai_model(streaming:bool = False, temperature:int = 0):
 
 def get_google_reasoning_model(streaming:bool = False, temperature:int = 0):
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-pro-exp-03-25",
+        model="gemini-2.5-pro-latest",
         temperature=temperature,
-        max_tokens=65536,
+        max_tokens=8192,
         disable_streaming=(not streaming)
     )
 
 def get_google_model(streaming:bool = False, temperature:int = 0):
     return ChatGoogleGenerativeAI(
-        model="gemini-2.0-flash",
+        model="gemini-2.0-flash-latest",
         temperature=temperature,
         max_tokens=8192,
         disable_streaming=(not streaming)
